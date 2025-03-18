fd26b4343b043e5e3c5c6e8d3b7afddac5810417
Dênis Rufino <denis.rufino@gmail.com>
2025-03-18 20:55:35 -0300

    Adicionando JS

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos=[]
+
+function adicionarAmigo() {
+    const nome = document.getElementById('amigo').value;
+
+
+    if (nome !== "" && !amigos.includes(nome)) {
+        amigos.push(nome);
+        exibirListaAmigos()
+        document.getElementById('amigo').value = ""; 
+    } else if (amigos.includes(nome)) {
+        alert("Esse nome já foi adicionado. Tente outro.");
+    } else {
+        alert("Por favor, digite um nome válido.");
+    }
+}
+function exibirListaAmigos() {
+    const listaAmigosDiv = document.getElementById('listaAmigos');
+    listaAmigosDiv.innerHTML = "";
+
+    amigos.forEach(amigo => {
+        const amigoSecreto = document.createElement('li');
+        amigoSecreto.textContent = amigo;
+        listaAmigosDiv.appendChild(amigoSecreto);
+    });
+}
+
+function sortearAmigo() {
+    
+    if (amigos.length === 0) {
+        alert("Não há amigos para sortear.");
+        return;  
+    } 
+    const indiceSorteado = Math.floor(Math.random() * amigos.length);
+    const amigoSorteado = amigos[indiceSorteado];
+
+    alert(`O amigo secreto sorteado é: ${amigoSorteado}`);
+    amigos.splice(indiceSorteado, 1);
+    exibirListaAmigos()
+}
